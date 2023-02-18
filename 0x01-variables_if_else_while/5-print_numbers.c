0e6beeb524173a3aed58cc754d1735d21489611d
beminelz <moyin.lamija@gmail.com>
2023-02-18 07:57:19 -0800

    Task 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Programs that prints single digit base 0 to 10
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+
+}
