0795e101309035120bff2e046488b9538b75ddc9
beminelz <moyin.lamija@gmail.com>
2023-02-18 05:22:05 -0800

    Task 101-quote.c updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - C program: prints a line to the standard error
- * Return: 1
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
