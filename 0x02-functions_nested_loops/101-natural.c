e6e0d47103117a72d65beabf6873039e4a5042b3
beminelz <moyin.lamija@gmail.com>
2023-02-22 04:47:23 -0800

    Task 13. Nature made the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Programs that prints multiples of 3 and 5 below 1024
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+	if ((x % 3 == 0) || (x % 5 == 0))
+	{
+	y += x;
+	}
+	x++;
+	}
+	printf("%d\n", y);
+
+	return (0);
+
+}
