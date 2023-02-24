bd151a273a9e606270149b4e85cf0f7d8e26c50f
beminelz <moyin.lamija@gmail.com>
2023-02-24 11:44:05 -0800

    Task 1, isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,17 +2,17 @@
 
 /**
  * _isdigit - Check digit
- * @c: The number to be checked
+ * @c: char to be checked
+ *
  * Return: 0 or 1
  */
 
 int _isdigit(int c);
-
 {
 
 	if (c >= '0' && c <= '9')
-	return (1);
+		return (1);
 	else
-	return (0);
+		return (0);
 
 }
