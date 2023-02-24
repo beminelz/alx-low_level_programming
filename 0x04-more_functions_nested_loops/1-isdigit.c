42591db6f28076067af8f486a5b0a1a70076814f
beminelz <moyin.lamija@gmail.com>
2023-02-24 11:32:12 -0800

    Task 1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check digit
+ * @c: The number to be checked
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c);
+
+{
+
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+
+}
